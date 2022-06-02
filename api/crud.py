ffd69438a5c7e1b2513b775c7df3843998ed7569
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-06-02 19:24:35 +0200

    Moved to python10, added get & get with id to Status and Level model

--- api/crud.py
+++ api/crud.py
@@ -2,8 +2,23 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 
+#Tickets
 def get_ticket(db: Session, ticket_id: int):
     return db.query(models.Ticket).filter(models.Ticket.id == ticket_id).first()
 
 def get_all_tickets(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Ticket).offset(skip).limit(limit).all()
+
+#Statuses
+def get_all_statuses(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Status).offset(skip).limit(limit).all()
+
+def get_status(db: Session, status_id: int):
+    return db.query(models.Status).filter(models.Status.id == status_id).first()
+
+#Levels
+def get_all_levels(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Level).offset(skip).limit(limit).all()
+
+def get_level(db: Session, level_id: int):
+    return db.query(models.Level).filter(models.Level.id == level_id).first()
--- api/main.py
+++ api/main.py
@@ -15,6 +15,7 @@ def get_db():
     finally:
         db.close()
 
+#Tickets
 @app.get('/tickets/', response_model=list[schemas.Ticket])
 def read_all_tickets(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     all_tickets = crud.get_all_tickets(db, skip=skip, limit=limit)
@@ -26,5 +27,37 @@ def read_ticket(ticket_id: int, db: Session = Depends(get_db)):
     if ticket is None:
         raise HTTPException(status_code=404, detail="Ticket not found")
     return ticket
+
+#Statuses
+@app.get('/statuses/', response_model=list[schemas.Status])
+def read_all_statuses(skip: int = 0, limit: int=100, db: Session = Depends(get_db)):
+    all_statuses = crud.get_all_statuses(db, skip=skip, limit=limit)
+    return all_statuses
+
+@app.get("/statuses/{status_id}", response_model=schemas.Status)
+def read_status(status_id: int, db: Session = Depends(get_db)):
+    status = crud.get_status(db, status_id=status_id)
+    if status is None:
+        raise HTTPException(status_code=404, detail="Status not found")
+    return status
+
+
+#Levels
+@app.get('/levels/', response_model=list[schemas.Level])
+def read_all_levels(skip: int = 0, limit: int=100, db: Session = Depends(get_db)):
+    all_levels = crud.get_all_levels(db, skip=skip, limit=limit)
+    return all_levels
+
+@app.get("/levels/{level_id}", response_model=schemas.Level)
+def read_level(level_id: int, db: Session = Depends(get_db)):
+    level = crud.get_level(db, level_id=level_id)
+    if level is None:
+        raise HTTPException(status_code=404, detail="Level not found")
+    return level
+
+
+#TODO GET TICKETTYPES
+
+
 
     
