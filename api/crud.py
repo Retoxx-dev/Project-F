3d6b140848114abe4c644777a6f3881d6bb3f6af
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-06-04 17:18:36 +0200

    Added ticket, status, level, tickettype post methods

--- api/crud.py
+++ api/crud.py
@@ -9,12 +9,36 @@ def get_ticket(db: Session, ticket_id: int):
 def get_all_tickets(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Ticket).offset(skip).limit(limit).all()
 
+def post_ticket(db: Session, ticket: schemas.TicketCreate):
+    ticket = models.Ticket(
+        summary = ticket.summary,
+        description = ticket.description,
+        status_id = ticket.status_id,
+        level_id = ticket.level_id,
+        ticket_type_id = ticket.ticket_type_id,
+        closure_note = ticket.closure_note,
+        )
+    db.add(ticket)
+    db.commit()
+    db.refresh(ticket)
+    return ticket
+
 #Statuses
 def get_all_statuses(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Status).offset(skip).limit(limit).all()
 
 def get_status(db: Session, status_id: int):
     return db.query(models.Status).filter(models.Status.id == status_id).first()
+
+def get_status_name(db: Session, status_name: str):
+    return db.query(models.Status).filter(models.Status.status_name == status_name).first()
+
+def post_status(db: Session, status: schemas.StatusCreate):
+    status = models.Status(status_name = status.status_name)
+    db.add(status)
+    db.commit()
+    db.refresh(status)
+    return status
 
 #Levels
 def get_all_levels(db: Session, skip: int = 0, limit: int = 100):
@@ -23,9 +47,29 @@ def get_all_levels(db: Session, skip: int = 0, limit: int = 100):
 def get_level(db: Session, level_id: int):
     return db.query(models.Level).filter(models.Level.id == level_id).first()
 
+def get_level_name(db: Session, level_name: str):
+    return db.query(models.Level).filter(models.Level.level_name == level_name).first()
+
+def post_level(db: Session, level: schemas.LevelCreate):
+    level = models.Level(level_name = level.level_name)
+    db.add(level)
+    db.commit()
+    db.refresh(level)
+    return level
+
 #TicketTypes
 def get_all_ticket_types(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.TicketType).offset(skip).limit(limit).all()
 
 def get_ticket_type(db: Session, ticket_type_id: int):
     return db.query(models.TicketType).filter(models.TicketType.id == ticket_type_id).first()
+
+def get_ticket_type_name(db: Session, ticket_type_name: str):
+    return db.query(models.TicketType).filter(models.TicketType.ticket_type_name == ticket_type_name).first()
+
+def post_ticket_type(db: Session, ticket_type: schemas.TicketTypeCreate):
+    ticket_type = models.TicketType(ticket_type_name = ticket_type.ticket_type_name)
+    db.add(ticket_type)
+    db.commit()
+    db.refresh(ticket_type)
+    return ticket_type
--- api/schemas.py
+++ api/schemas.py
@@ -1,11 +1,23 @@
 from pydantic import BaseModel
 from datetime import datetime
 
-class Ticket(BaseModel):
-    id: int
+#Ticket
+class TicketBase(BaseModel):
+    pass        
+  
+class TicketCreate(TicketBase):
     summary: str
     description: str
     username: str
+    status_id: int
+    level_id: int
+    ticket_type_id: int
+    closure_note: str | None = None
+    
+class Ticket(TicketBase):
+    id: int
+    summary: str
+    description: str
     status_id: int
     level_id: int
     ticket_type_id: int
@@ -15,21 +27,42 @@ class Ticket(BaseModel):
     class Config:
         orm_mode = True
         
-class Status(BaseModel):
+#Status
+class StatusBase(BaseModel):
+    pass        
+  
+class StatusCreate(StatusBase):
+    status_name: str
+    
+class Status(StatusBase):
     id: int
     status_name: str
     
     class Config:
         orm_mode = True
-        
-class Level(BaseModel):
+            
+#Level
+class LevelBase(BaseModel):
+    pass   
+
+class LevelCreate(LevelBase):
+    level_name: str
+
+class Level(LevelBase):
     id: int
     level_name: str
     
     class Config:
         orm_mode = True
 
-class TicketType(BaseModel):
+#TicketType
+class TicketTypeBase(BaseModel):
+    pass   
+
+class TicketTypeCreate(TicketTypeBase):
+    ticket_type_name: str
+    
+class TicketType(TicketTypeBase):
     id: int
     ticket_type_name: str
     
