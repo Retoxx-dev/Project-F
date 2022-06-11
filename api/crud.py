d15caffb28ae8f2d39cc498f0021a933355ba2a2
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-06-11 21:42:14 +0200

    Added put and delete requests

--- api/crud.py
+++ api/crud.py
@@ -62,6 +62,26 @@ def post_status(db: Session, status: schemas.StatusCreate):
     db.refresh(status)
     return status
 
+def delete_status(db: Session, status_id: int):
+    status_to_delete = db.query(models.Status).filter(models.Status.id == status_id).first()
+    db.delete(status_to_delete)
+    db.commit()
+    return schemas.ResponseModel(details=f"Status {status_id} has been deleted")
+
+def put_status(db: Session, db_obj, obj_in):
+    obj_data = jsonable_encoder(db_obj)
+    if isinstance(obj_in, dict):
+        update_data = obj_in
+    else:
+        update_data = obj_in.dict(exclude_unset=True)
+    for field in obj_data:
+        if field in update_data:
+            setattr(db_obj, field, update_data[field])
+    db.add(db_obj)
+    db.commit()
+    db.refresh(db_obj)
+    return db_obj
+
 #Levels
 def get_all_levels(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Level).offset(skip).limit(limit).all()
@@ -79,6 +99,26 @@ def post_level(db: Session, level: schemas.LevelCreate):
     db.refresh(level)
     return level
 
+def delete_level(db: Session, level_id: int):
+    level_to_delete = db.query(models.Level).filter(models.Level.id == level_id).first()
+    db.delete(level_to_delete)
+    db.commit()
+    return schemas.ResponseModel(details=f"Status {level_id} has been deleted")
+
+def put_level(db: Session, db_obj, obj_in):
+    obj_data = jsonable_encoder(db_obj)
+    if isinstance(obj_in, dict):
+        update_data = obj_in
+    else:
+        update_data = obj_in.dict(exclude_unset=True)
+    for field in obj_data:
+        if field in update_data:
+            setattr(db_obj, field, update_data[field])
+    db.add(db_obj)
+    db.commit()
+    db.refresh(db_obj)
+    return db_obj
+
 #TicketTypes
 def get_all_ticket_types(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.TicketType).offset(skip).limit(limit).all()
@@ -95,3 +135,23 @@ def post_ticket_type(db: Session, ticket_type: schemas.TicketTypeCreate):
     db.commit()
     db.refresh(ticket_type)
     return ticket_type
+
+def delete_ticket_type(db: Session, ticket_type_id: int):
+    ticket_type_to_delete = db.query(models.TicketType).filter(models.TicketType.id == ticket_type_id).first()
+    db.delete(ticket_type_to_delete)
+    db.commit()
+    return schemas.ResponseModel(details=f"Ticket type {ticket_type_id} has been deleted")
+
+def put_ticket_type(db: Session, db_obj, obj_in):
+    obj_data = jsonable_encoder(db_obj)
+    if isinstance(obj_in, dict):
+        update_data = obj_in
+    else:
+        update_data = obj_in.dict(exclude_unset=True)
+    for field in obj_data:
+        if field in update_data:
+            setattr(db_obj, field, update_data[field])
+    db.add(db_obj)
+    db.commit()
+    db.refresh(db_obj)
+    return db_obj
