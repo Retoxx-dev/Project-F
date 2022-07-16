4304c6df2378eaca90453d3d1ca33f0a4bb19725
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-07-16 20:53:13 +0200

    Added terraform to the project

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-projectf-terraform-dev-001"
+    storage_account_name = "saprojectfstatedev001"
+    container_name       = "state-project-f-dev-001"
+    key                  = "projectf-dev-001.tfstate"
+  }
+}
+
+provider "azurerm" {
+  version = "3.14.0"
+  features {}
+}
+ 
+data "azurerm_client_config" "current" {}
+ 
+#Create Resource Group
+resource "azurerm_resource_group" "rg-projectf-dev-001" {
+  name     = "rg-projectf-dev-001"
+  location = "northeurope"
+}
