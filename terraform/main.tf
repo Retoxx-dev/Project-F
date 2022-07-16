aba3c16cbdc6add51b2f330f94b303ea41dd4367
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-07-16 20:36:38 +0200

    Added terraform

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-projectf-terraform-dev-001"
+    storage_account_name = "saprojectfstatedev001"
+    container_name       = "state-project-f-dev-001"
+    key                  = "projectf-dev-001.tfstate"
+  }
+}
+
+provider "azurerm" {
+  version = "~>2.0"
+  features {}
+}
+ 
+data "azurerm_client_config" "current" {}
+ 
+#Create Resource Group
+resource "azurerm_resource_group" "tamops" {
+  name     = "tamops"
+  location = "eastus2"
+}
