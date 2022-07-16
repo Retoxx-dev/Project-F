355959c4228cabdb02566a30fb4229aa61322767
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-07-16 20:39:11 +0200

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,6 @@ data "azurerm_client_config" "current" {}
  
 #Create Resource Group
 resource "azurerm_resource_group" "tamops" {
-  name     = "tamops"
+  name     = "tamops2"
   location = "eastus2"
 }
