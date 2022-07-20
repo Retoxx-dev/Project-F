e960da8734b33ebc918194e3b3788b7ee47be6ca
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-07-20 11:02:14 +0200

    Fixed service principal password issue

--- terraform/serviceprincipal.tf
+++ terraform/serviceprincipal.tf
@@ -1,21 +1,20 @@
 data "azuread_client_config" "current" {}
 
+#Random string - create SP password
+resource "random_string" "service-principal-password" {
+  length  = 64
+  special = true
+}
+
 resource "azuread_application" "ACR-service-principal" {
   display_name = var.acr-service-principal-name
   owners       = [data.azuread_client_config.current.object_id]
 }
 
 resource "azuread_service_principal_password" "ACR-service-principal-password" {
-  display_name         = var.acr-service-principal-password-name
   service_principal_id = azuread_application.ACR-service-principal.id
-  end_date    = "2022-09-01T00:00:00Z"
+  end_date    = "2022-09-01T01:02:03Z"
   value                = random_string.service-principal-password.result
-}
-
-#Random string - create SP password
-resource "random_string" "service-principal-password" {
-  length  = 64
-  special = true
 }
 
 #Output
