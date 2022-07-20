f3a85f78f2aa8e6cca5eead9894640c8cccc3ced
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-07-20 11:12:49 +0200

    Fixed reference issues

--- terraform/serviceprincipal.tf
+++ terraform/serviceprincipal.tf
@@ -1,11 +1,5 @@
 data "azuread_client_config" "current" {}
 
-#Random string - create SP password
-resource "random_string" "application-password" {
-  length  = 32
-  special = true
-}
-
 resource "azuread_application" "ACR-application" {
   display_name = var.acr-service-principal-name
   owners       = [data.azuread_client_config.current.object_id]
@@ -13,7 +7,6 @@ resource "azuread_application" "ACR-application" {
 
 resource "azuread_application_password" "ACR-application-password" {
   application_object_id = azuread_application.ACR-application.id
-  value                = random_string.application-password.result
   end_date    = "2299-12-31T00:00:00Z"
 }
 
