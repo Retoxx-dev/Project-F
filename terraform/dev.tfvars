87d6247eb8ea0dc4bcba54496fa9954176083875
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-07-20 11:38:43 +0200

    Fixed githubactions terraform

--- terraform/dev.tfvars
+++ terraform/dev.tfvars
@@ -6,6 +6,6 @@ acr-service-principal-name = "GithubActionsAcrAccess"
 
 acr-service-principal-password-name = "GithubActionsAcrAccessSecret"
 
-github-repository-name = "Project-F"
+github-repository-name = "Retoxx-dev/Project-F"
 
 service-principal-github-secret-name = "GithubActionsAcrAccessSecret"
--- terraform/githubreposecrets.tf
+++ terraform/githubreposecrets.tf
@@ -1,5 +1,8 @@
+data "github_repository" "main-repo" {
+  full_name = var.github-repository-name
+}
 resource "github_actions_secret" "service-principal-password-github-secret" {
-  repository       = var.github-repository-name
+  repository       = data.github_repository.main-repo.name
   secret_name      = var.service-principal-github-secret-name
   plaintext_value  = azuread_application_password.ACR-application-password.value
 }
