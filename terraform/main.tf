fd4abdcc31ea5369adc1a480cc787791dcc815c5
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-07-20 10:33:51 +0200

    Terraform changes

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-projectf-terraform-dev-001"
+    storage_account_name = "saprojectfstatedev001"
+    container_name       = "state-project-f-dev-001"
+    key                  = "projectf-dev-001.tfstate"
+  }
+}
+
+provider "azurerm" {
+  version = "3.14.0"
+  features {}
+}
