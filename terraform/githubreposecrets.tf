39506b17500a4f3dc645342943d81cec063d7282
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-07-20 11:42:16 +0200

    Fixed githubactions terraform

--- terraform/githubreposecrets.tf
+++ terraform/githubreposecrets.tf
@@ -1,8 +1,5 @@
-data "github_repository" "main-repo" {
-  full_name = var.github-repository-name
-}
 resource "github_actions_secret" "service-principal-password-github-secret" {
-  repository       = data.github_repository.main-repo.name
+  repository       = var.github-repository-name
   secret_name      = var.service-principal-github-secret-name
   plaintext_value  = azuread_application_password.ACR-application-password.value
 }
