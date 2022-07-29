bbe7a12341885f03dd9589ae50acb0503c71b87f
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-07-29 16:39:47 +0200

    Fixed typo

--- terraform/serviceprincipal.tf
+++ terraform/serviceprincipal.tf
@@ -5,7 +5,7 @@ resource "azuread_application" "ACR-access-app" {
 }
 
 resource "azuread_application_password" "ACR-access-app-password" {
-  display_name         = var.acr-application-access-secret-name
+  display_name         = var.acr-access-secret-name
   application_object_id = azuread_application.ACR-access-app.id
   end_date    = "2022-09-01T01:02:03Z"
 }
