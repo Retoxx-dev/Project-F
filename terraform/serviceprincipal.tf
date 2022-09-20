a16fc61a7897501ccebf97dd85a0577e48c966cf
Kacper Dziedzic <59125920+Retoxx-dev@users.noreply.github.com>
2022-09-21 00:47:14 +0200

    Deleted service principal data block

--- terraform/serviceprincipal.tf
+++ terraform/serviceprincipal.tf
@@ -11,7 +11,3 @@ resource "azuread_service_principal" "ACR-access-sp" {
 resource "azuread_service_principal_password" "ACR-access-sp-password" {
   service_principal_id = azuread_service_principal.ACR-access-sp.id
 }
-
-data "azuread_service_principal" "data-ACR-access-service-principal" {
-  application_id = azuread_service_principal.ACR-access-sp.id
-}
